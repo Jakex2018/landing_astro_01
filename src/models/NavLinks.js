6069e75c0a948da922415114a67d5aa1b2da663a
Jakex2018 <pusugu00@gmail.com>
2024-12-15 23:34:32 -0400

    first commit

---
+++ src/models/NavLinks.js
@@ -0,0 +1,16 @@
+export const NavLinks = [
+    {
+        title: 'Product',
+    },
+    {
+        title: 'Recipe',
+    },
+    {
+        title: 'About',
+    }
+]
+
+export default NavLinks
+
+
+
---
+++ src/models/SaladCard.js
@@ -0,0 +1,17 @@
+export const SaladCard = [
+    {
+        image: '/asset/asian01.png',
+    },
+    {
+        image: '/asset/asian02.png',
+    },
+    {
+        image: '/asset/asian03.png',
+    },
+    {
+        image: '/asset/asian04.png',
+    },
+
+]
+
+export default SaladCard
